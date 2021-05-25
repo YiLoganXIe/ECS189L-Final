add8e070664015326bf3685ca34a6ca4d1359848
ChrissyKX <christineeee1220@gmail.com>
2021-05-25 16:11:00 +0800

    Lerp Effect

--- PartyTime/Assets/Script/Monster/BossAbsorbingController.cs
+++ PartyTime/Assets/Script/Monster/BossAbsorbingController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class BossAbsorbingController : MonoBehaviour
 {
     private List<GameObject> AbsorbingObjects = new List<GameObject>();
-    [SerializeField] private float LerpInterval = 5f;
+    private List<float> CurLerpTimes = new List<float>();
+    [SerializeField] private float LerpAhead = 10f;
+    private float DescendingTime = 2;
+    private float DesDistance = 150;
+    private float CurTime = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +24,14 @@ public class BossAbsorbingController : MonoBehaviour
         {
             this.Absorb(obj);
         }
-        
+        if (CurTime < DescendingTime)
+        {
+            this.transform.position -= new Vector3(0f, Time.deltaTime / this.DescendingTime * DesDistance, 0f);
+            CurTime += Time.deltaTime;
+        }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         Debug.Log("On Trigger From Boss!");
         if (other.gameObject.CompareTag("SuckableObject"))
@@ -38,13 +46,14 @@ public class BossAbsorbingController : MonoBehaviour
         if (!this.AbsorbingObjects.Contains(obj))
         {
             this.AbsorbingObjects.Add(obj);
+            this.CurLerpTimes.Add(0f);
         }
     }
 
     private void Absorb(GameObject obj)
     {
         var curPosition = obj.transform.position;
-        var nextPosition = Vector3.Lerp(curPosition, this.transform.position, this.LerpInterval);
+        var nextPosition = Vector3.Lerp(curPosition, this.transform.position, 1f / this.LerpAhead * Time.deltaTime);
         obj.transform.position = nextPosition;
     }
 }
