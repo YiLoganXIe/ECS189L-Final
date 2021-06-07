b9efcf72c922c1719ae65250df6b0bdd47074cbd
yiloganxie <loganxieyi@gmail.com>
2021-06-06 17:27:57 -0700

    Light Particle Update

---
+++ PartyTime/Assets/Script/UI/ChangeNote.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChangeNote : MonoBehaviour
+{
+    private GameObject Player;
+    private GameObject Note;
+    private bool step1 = false;
+    private bool step2 = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Player = GameObject.FindWithTag("Player");
+        this.Note = GameObject.FindGameObjectWithTag("QuestNote");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (step1)
+        {
+            if (this.Player == null)
+            {
+                this.Player = GameObject.FindWithTag("Player");
+            }
+            else
+            {
+                if (Note == null)
+                {
+                    Debug.Log("Cannot Find Note");
+                    this.Note = GameObject.FindGameObjectWithTag("QuestNote");
+                }
+                else
+                {
+                    //Debug.Log(Note.GetComponent<TextMeshProUGUI>().text);
+                    Note.GetComponent<TMP_InputField>().text = $"Collect Memory Shards: {this.Player.GetComponent<PlayerController>().GetNumPlayerParticles()}";
+                }
+            }
+        }
+    }
+
+    public void openNote()
+    {
+        Debug.Log("Clicked NotePad!");
+        if (!step1)
+            step1 = true;
+    }
+}
